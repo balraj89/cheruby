95a03cd54fbedaf21ffe539de5542b11d9a0e05b
balraj89 <43946571+balraj89@users.noreply.github.com>
2023-07-10 04:41:00 +0530

    Add files via upload

---
+++ chess/js/CMenu.js
@@ -0,0 +1,188 @@
+function CMenu() {
+    var _oBg;
+    var _oButPlay;
+    var _oFade;
+    var _oAudioToggle;
+    var _oButCredits;
+    var _oButFullscreen;
+    var _oLogo;
+    var _fRequestFullScreen = null;
+    var _fCancelFullScreen = null;
+
+    var _pStartPosCredits;
+    var _pStartPosFullscreen;
+    var _pStartPosAudio;
+    var _pStartPosPlay;
+
+    this._init = function() {
+        _oBg = createBitmap(s_oSpriteLibrary.getSprite('bg_menu'));
+        s_oStage.addChild(_oBg);
+
+        var oSprite = s_oSpriteLibrary.getSprite('logo_menu');
+        _oLogo = createBitmap(oSprite);
+        _oLogo.regX = oSprite.width / 2;
+        _oLogo.regY = oSprite.height / 2;
+        _oLogo.x = CANVAS_WIDTH / 2;
+        _oLogo.y = CANVAS_HEIGHT / 2;
+        _oLogo.scaleX = _oLogo.scaleY = 0.8;
+        s_oStage.addChild(_oLogo);
+
+        var oSprite = s_oSpriteLibrary.getSprite('but_play');
+        _pStartPosPlay = {
+            x: CANVAS_WIDTH - oSprite.width / 2 - 10,
+            y: CANVAS_HEIGHT - oSprite.height / 2 - 10
+        };
+        _oButPlay = new CGfxButton(_pStartPosPlay.x, _pStartPosPlay.y, oSprite, s_oStage);
+        _oButPlay.addEventListener(ON_MOUSE_UP, this._onButPlayRelease, this);
+        _oButPlay.pulseAnimation();
+
+        var oSprite = s_oSpriteLibrary.getSprite('but_credits');
+        _pStartPosCredits = {
+            x: 10 + oSprite.width / 2,
+            y: (oSprite.height / 2) + 25
+        };
+        _oButCredits = new CGfxButton(_pStartPosCredits.x, _pStartPosCredits.y, oSprite, s_oStage);
+        _oButCredits.addEventListener(ON_MOUSE_UP, this._onCredits, this);
+
+        if (DISABLE_SOUND_MOBILE === false || s_bMobile === false) {
+            var oSprite = s_oSpriteLibrary.getSprite('audio_icon');
+            _pStartPosAudio = {
+                x: CANVAS_WIDTH - (oSprite.height / 2) - 10,
+                y: (oSprite.height / 2) + 25
+            };
+            _oAudioToggle = new CToggle(_pStartPosAudio.x, _pStartPosAudio.y, oSprite, s_bAudioActive, s_oStage);
+            _oAudioToggle.addEventListener(ON_MOUSE_UP, this._onAudioToggle, this);
+        }
+
+        var doc = window.document;
+        var docEl = doc.documentElement;
+        _fRequestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+        _fCancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+        if (ENABLE_FULLSCREEN === false) {
+            _fRequestFullScreen = false;
+        }
+
+        if (_fRequestFullScreen && screenfull.enabled) {
+            oSprite = s_oSpriteLibrary.getSprite('but_fullscreen');
+            _pStartPosFullscreen = {
+                x: _pStartPosCredits.x + oSprite.width / 2 + 10,
+                y: _pStartPosCredits.y
+            };
+
+            _oButFullscreen = new CToggle(_pStartPosFullscreen.x, _pStartPosFullscreen.y, oSprite, s_bFullscreen, s_oStage);
+            _oButFullscreen.addEventListener(ON_MOUSE_UP, this._onFullscreenRelease, this);
+        }
+
+        _oFade = new createjs.Shape();
+        _oFade.graphics.beginFill("black").drawRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        s_oStage.addChild(_oFade);
+
+        createjs.Tween.get(_oFade).to({
+            alpha: 0
+        }, 1000).call(function() {
+            _oFade.visible = false;
+        });
+
+        this.refreshButtonPos();
+
+    };
+
+    this.unload = function() {
+        _oButPlay.unload();
+        _oButPlay = null;
+        _oFade.visible = false;
+
+        if (DISABLE_SOUND_MOBILE === false || s_bMobile === false) {
+            _oAudioToggle.unload();
+            _oAudioToggle = null;
+        }
+        _oButCredits.unload();
+        if (_fRequestFullScreen && screenfull.enabled) {
+            _oButFullscreen.unload();
+        }
+
+        s_oStage.removeChild(_oBg);
+        _oBg = null;
+        s_oMenu = null;
+    };
+
+    this.refreshButtonPos = function() {
+        if (DISABLE_SOUND_MOBILE === false || s_bMobile === false) {
+            _oAudioToggle.setPosition(_pStartPosAudio.x - s_iOffsetX, s_iOffsetY + _pStartPosAudio.y);
+        }
+        _oButCredits.setPosition(_pStartPosCredits.x + s_iOffsetX, s_iOffsetY + _pStartPosCredits.y);
+
+        if (_fRequestFullScreen && screenfull.enabled) {
+            _oButFullscreen.setPosition(_pStartPosFullscreen.x + s_iOffsetX, s_iOffsetY + _pStartPosFullscreen.y);
+        }
+
+        if (s_bLandscape) {
+            var oSprite = s_oSpriteLibrary.getSprite('but_play');
+            _pStartPosPlay = {
+                x: CANVAS_WIDTH - oSprite.width / 2 - 10,
+                y: CANVAS_HEIGHT - oSprite.height / 2 - 10
+            };
+            _oButPlay.setPosition(_pStartPosPlay.x - s_iOffsetX, _pStartPosPlay.y - s_iOffsetY);
+        } else {
+            var oSprite = s_oSpriteLibrary.getSprite('but_play');
+            _pStartPosPlay = {
+                x: CANVAS_WIDTH / 2,
+                y: CANVAS_HEIGHT - oSprite.height / 2 - 200
+            };
+            _oButPlay.setPosition(_pStartPosPlay.x, _pStartPosPlay.y - s_iOffsetY);
+        }
+
+
+    };
+
+    this._onAudioToggle = function() {
+        Howler.mute(s_bAudioActive);
+        s_bAudioActive = !s_bAudioActive;
+    };
+
+    this._onButPlayRelease = function() {
+        if (s_bFirstTimePlay) {
+            s_oMain.pokiShowCommercial(s_oMenu._startGame);
+        } else {
+            s_oMenu._startGame();
+        }
+    };
+
+    this._startGame = function() {
+        s_oMenu.unload();
+
+        $(s_oMain).trigger("start_session");
+
+        s_oMain.gotoModeMenu();
+
+        s_bFirstTimePlay = false;
+    };
+
+    this._onCredits = function() {
+        new CCreditsPanel();
+    };
+
+    this.resetFullscreenBut = function() {
+        if (_fRequestFullScreen && screenfull.enabled) {
+            _oButFullscreen.setActive(s_bFullscreen);
+        }
+    };
+
+    this._onFullscreenRelease = function() {
+        if (s_bFullscreen) {
+            _fCancelFullScreen.call(window.document);
+        } else {
+            _fRequestFullScreen.call(window.document.documentElement);
+        }
+
+        sizeHandler();
+    };
+
+    s_oMenu = this;
+
+    this._init();
+}
+
+var s_oMenu = null;
